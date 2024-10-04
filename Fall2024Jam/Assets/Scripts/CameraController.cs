7d3d44abcc4d592764934cdc22fcbbdd6c1450b3
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-10-04 12:41:31 -0500

    Added Test Scene and Camera Controller

---
+++ Fall2024Jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    public CinemachineVirtualCamera activeCam;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player")){
+            activeCam.Priority = 1;
+        }
+    }
+
+     private void OnTriggerExit(Collider other){
+        if(other.CompareTag("Player")){
+             activeCam.Priority = 0;
+        }
+    }
+}
