19235efa6cbddb1e97f722ef7317a54d766d93d2
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-10-09 14:48:32 -0500

    created PlayerHide script
    created PlayerHide script, which is from a tutorial

---
+++ Fall2024Jam/Assets/Scripts/PlayerHide.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHide : MonoBehaviour
+{
+    public GameObject hideText, stopHideText;
+    public GameObject normalPlayer, hidingPlayer;
+    public enemyAI monsterScript; 
+    public interactable hiding;
+    public float loseDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = false;
+        hiding = false;
+    }
+
+    void OnTriggerStay(Collider other){
+        if (other.CompareTag("MainCamera"))
+        {
+            hideText.SetActive(true);
+            interactable(true;)
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            hideText.SetActive(false);
+            interatable = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(interactable == true)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                hidingText.SetActive(false);
+                hidingPlayer.SetActive(true);
+                float distance = Vector3.Distance(monsterTransform, normalPlayer.transformposition);
+                if(distance > loseDistance)
+                {
+                    if(monsterScript.chasing== true)
+                    {
+                        monsterScript.stopChase();
+                    }
+                }
+            }
+        }
+    }
+}
