050e3a604b7150a70255241847e254c38c7b655b
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-10-11 21:08:25 -0500

    Added scripts RayCastInteract and IInteractable
    RayCastInteract is responsible for detecting interactable objects within a specified distance from the player using raycasting from a fixed camera's viewpoint. When an object that implements the IInteractable interface is detected, it is added to a list of interactable objects, allowing for potential interactions to be processed in subsequent logic.

---
+++ Fall2024Jam/Assets/Scripts/Managers/KeyInventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScaredyKat.Managers
+{
+    public class KeyInventroy : MonoBehaviour
+    {
+        public bool hashedKey = false;
+    }
+}
